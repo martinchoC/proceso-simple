264c20e64cbbe508511076e061cf758ef6e3e972
Martin C. <martin.e.castro.88@gmail.com>
2025-12-21 15:34:34 -0300

    Actualizar BASE_URL en config.php para reflejar la nueva estructura del proyecto

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php en la raíz del proyecto
 define('ROOT_PATH', dirname(__DIR__));
-define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/gestion_multipyme'); // Ajusta 'tu_proyecto' según tu estructura
+define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/gestion_multipyme_cp'); // Ajusta 'tu_proyecto' según tu estructura
 
 function asset($path) {
     return BASE_URL . '/templates/adminlte/' . ltrim($path, '/');
