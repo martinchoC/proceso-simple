6f77482d22fe7179ffb26fbb61d3e0c59beabeeb
Martin C. <martin.e.castro.88@gmail.com>
2026-01-07 01:21:29 -0300

    test: Add database connection test and remove generic PHP info.

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-echo "PHP está funcionando";
-phpinfo();
+$host = 'localhost';
+$user = 'u368960646_gestion';
+$pass = 'Coco3948';
+$db = 'u368960646_gestion';
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    echo "Fallo total: " . mysqli_connect_error();
+} else {
+    echo "¡CONEXIÓN EXITOSA! El problema estaba en el archivo .env";
+}
